730b2037843bee74fc9ab82fd053d4dfb0045a2f
Daywison <daywison@unsoft.com.br>
2023-01-12 21:53:56 -0300

    Aula numero 16

--- fundamentos-react/src/components/basicos/Primeiro.js
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,9 +1,11 @@
 import React from "react"
 
-export default function Prrimeiro(){
+export default function Prrimeiro() {
+    const msg = 'hello world'
     return(
         <div>
             <h1>Primeiro component jsx</h1>
+            <p> { msg }  </p>
         </div>
         
     )
