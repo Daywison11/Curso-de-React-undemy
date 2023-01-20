f620c75836be6a5796a1a99e73dd03b6c7c5b3ae
Daywison <daywison@unsoft.com.br>
2023-01-19 21:21:52 -0300

    Finalizando aula n 19

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,24 +1,28 @@
 import ReactDom from 'react-dom';
 //importação para funcionar a sintaxe JSX
-import React from 'react';
+import React, { Fragment } from 'react';
 
 //referenciando arquivo css 
 import './index.css';
 
 //importando components
-import Prrimeiro from './components/basicos/Primeiro';
+import Primeiro from './components/basicos/Primeiro';
 //Componente com parametro.
 import ComParametro from './components/basicos/ComParametro';
 
+import Fragmento from './components/basicos/Fragmento';
 
 ReactDom.render(
     <div> 
-        <Prrimeiro></Prrimeiro>
+        <Primeiro></Primeiro>
         <ComParametro
             titulo="Situação do aluno"
             aluno="João"
             nota={7.3} //dessa forma ele vai interpretar o parametro como um numero.
         />
+
+        <Fragmento/>
+        
 
     </div>,
     document.getElementById('root')
